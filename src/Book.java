5d52be65f8f8c6664ffd174382c1813a3936dc00
dendariev <den.dariev@gmail.com>
2022-12-19 02:29:16 +0800

    Готовое задание к уроку "Методы и классы"

---
+++ src/Book.java
@@ -0,0 +1,46 @@
+import java.util.Objects;
+
+public class Book {
+    private String name;
+    private Author author;
+    private int year;
+
+    public Book(String name, Author author, int year) {
+        this.name = name;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public boolean equals (Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(name, book.name) && author.equals(book.author);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author author = new Author("Джон", "Толкин");
+        Author author1 = new Author("Стивен", "Кинг");
+        Book book = new Book("Властелин колец", author, 1954);
+        Book book1 = new Book("Зеленая миля", author1, 1996);
+        book.setYear(1955);
+        System.out.println("Название книги - " +book.getName());
+        System.out.println("Автор - " +author.getFirstName()+ " " +author.getLastName());
+        System.out.println("Год издания - " +book.getYear());
+        System.out.println("Название книги - " +book1.getName());
+        System.out.println("Автор - " +author1.getFirstName()+ " " +author1.getLastName());
+        System.out.println("Год издания - " +book1.getYear());
     }
 }
